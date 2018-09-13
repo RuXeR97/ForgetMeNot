320ebd84da813b9d5dd2ee08268b43a0d3c41e91
RuXeR97 <ruxer0097@gmail.com>
2018-09-14 00:32:49 +0200

    Next projects to application

---
+++ InfrastructureLayer/DataAccess/Repositories/Specific/Task/TaskRepository.cs
@@ -0,0 +1,72 @@
+﻿using DomainLayer.Models.Task;
+using System.Collections.Generic;
+using System.Data.SQLite;
+
+namespace InfrastructureLayer.DataAccess.Repositories.Specific.Task
+{
+    public class TaskRepository
+    {
+        private string _connectionString;
+        enum TypeOfExistence
+        {
+            DoesExistInDb,
+            DOesNotExistInDb
+        }
+
+        enum RequestType
+        {
+            Add,
+            Update,
+            Read,
+            Delete,
+            ConfirmAdd,
+            ConfirmDelete
+        }
+
+        public TaskRepository()
+        {
+
+        }
+
+        public TaskRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public IEnumerable<ITaskModel> GetAll()
+        {
+            List<TaskModel> taskModelList = new List<TaskModel>();
+
+            using (SQLiteConnection sqLiteConnection = new SQLiteConnection(_connectionString))
+            {
+                try
+                {
+                    string sql = "SELECT * FROM Tasks";
+                    sqLiteConnection.Open();
+                    using (SQLiteCommand cmd = new SQLiteCommand(sql, sqLiteConnection))
+                    {
+                        using (SQLiteDataReader reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                TaskModel taskModel = new TaskModel();
+                                taskModel.Description = reader["Description"].ToString();
+                                // blabla
+                                // 9 minuta, 44 sekunda
+
+                                taskModelList.Add(taskModel);
+                            }
+                        }
+                    }
+                }
+                catch (SQLiteException e)
+                {
+
+                }
+            }
+
+            return taskModelList;
+        }
+
+    }
+}
